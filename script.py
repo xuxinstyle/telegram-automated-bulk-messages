9d5a7749f10f93b54f8a1e027631d252853245bf
cian.xu <cian.xu@idreamsky.com>
2022-10-11 17:39:07 +0800

    提交发广告机器人

--- script.py
+++ script.py
@@ -1,29 +1,55 @@
+# coding=utf-8
 # Program to send bulk customized messages through Telegram Desktop application
 # Author @inforkgodara
 
 import pyautogui
+
+import pyperclip
 import pandas
 import time
 
+import os
+
+
+api_hash = '7d820d4557af57f57ae3c5d40524ce80'
+api_id = '11770907'
+
+# pip install pyinstaller
+# pyinstaller -F -c -i python.ico script.py  生成exe 的命令
+
 excel_data = pandas.read_excel('Recipients data.xlsx', sheet_name='Recipients')
 
-count = 0
+
 
 time.sleep(3)
 
-for column in excel_data['Username'].tolist():
+while True:
 
-      pyautogui.press('esc')
-      pyautogui.hotkey('ctrl', 'f')
-      time.sleep(1)
-      pyautogui.write(str(excel_data['Username'][count]));
-      pyautogui.press('enter')
-      time.sleep(2)
-      pyautogui.press('down')
-      pyautogui.press('enter')
-      pyautogui.write(str(excel_data['Message'][0]));
-      pyautogui.press('enter')
-      pyautogui.press('esc')
-      count = count + 1
+      print("open telegram ....")
+      os.startfile("D:\soft\Telegram Desktop\Telegram.exe")
+      print("open telegram start!")
+      count = 0
+      for column in excel_data['Username'].tolist():
+            pyautogui.press('esc')
+            pyautogui.hotkey('ctrl', 'f')
+            time.sleep(1)
+            pyautogui.write(str(excel_data['Username'][count]));
+            pyautogui.press('enter')
+            time.sleep(2)
+            pyautogui.press('down')
+            pyautogui.press('enter')
 
-print('The script executed successfully.')
+            msg = excel_data['Message'][0]
+
+            pyperclip.copy(msg)
+            pyautogui.hotkey('ctrl', 'v')
+            # pyautogui.write("nanshanxili  400mi  70fenzhong huanbao  guanshifuwu ");
+            pyautogui.press('enter')
+            pyautogui.press('esc')
+            print("send message :" + msg + " to " + str(excel_data['Username'][count]) + " success!")
+            count = count + 1
+
+
+      print('The script executed successfully. send message '+str(count)+'')
+      time.sleep(3600)
+
